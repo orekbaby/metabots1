6f20bcb4620e2e532e7aa57ef3c6366169b9811c
Folakemi Orekoya <orekoyafolakemi@gmail.com>
2024-05-08 10:56:36 +0100

    update recent changes n

--- components/metabots/Dashboard/YourWallet.tsx
+++ components/metabots/Dashboard/YourWallet.tsx
@@ -28,7 +28,7 @@ const YourWallet = () => {
           </div>
 
           {/* First and Second Column */}
-          <div className="flex gap-[100px]">
+          <div className="flex gap-[96px]">
             {/* First column */}
             <div className="flex flex-col gap-3 p-1">
               <p className="font-normal text-xs">0x9c8bB....6107D21</p>
