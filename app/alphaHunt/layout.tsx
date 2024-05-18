fa7fe2dfd86a16cfddc6e9b68dc5896164024fbf
Folakemi Orekoya <orekoyafolakemi@gmail.com>
2024-05-18 01:13:09 +0100

    update everything please b

--- app/alphaHunt/layout.tsx
+++ app/alphaHunt/layout.tsx
@@ -1,20 +1,9 @@
 import dynamic from "next/dynamic";
 
-const MetabotsTabs = dynamic(
-  () => import("@/components/metabots/MetabotsTabs"),
-  {
-    ssr: false,
-  }
-);
 interface DashbordLayoutProps {
   children: React.ReactNode;
 }
 
 export default async function MetaLayout({ children }: DashbordLayoutProps) {
-  return (
-    <div className="">
-      <MetabotsTabs />
-      {children}
-    </div>
-  );
+  return <div className="">{children}</div>;
 }
